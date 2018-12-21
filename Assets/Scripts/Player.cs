cedcc9e6652f58ed5e27daac80cda0b08e028f50
念麟 余 <yunianlin@outlook.com>
2018-12-21 13:51:06 +0800

    sac
    asc

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int  m_life = 3;
+    public float m_speed = 1;
+    public Transform m_rocket;
+protected Transform m_plane;
+    float m_rovketTimer = 0;
+	// Use this for initialization
+	void Start () {
+
+        m_plane = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movev = 0;
+        float moveh = 0;
+       
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            movev-= m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movev += m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveh += m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveh -= m_speed * Time.deltaTime;
+        }
+        m_rovketTimer -= Time.deltaTime;
+        if (m_rovketTimer <= 0)
+        {
+            m_rovketTimer = 0.1f;
+            if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+            {
+                Instantiate(m_rocket, m_plane.position, m_plane.rotation);
+            }
+        }
+        m_plane.Translate(new Vector3(moveh, 0, movev));
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.CompareTo("Enemy") == 0)
+        {
+            m_life -= 1;
+            if (m_life <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+    public static UI Instance;
+    public AudioClip m_musicClip;//背景音乐
+    public AudioSource m_Audio;//声音源
+    public Transform canvas;//开始界面
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+        m_Audio = this.gameObject.AddComponent<AudioSource>();//使用代码添加音效组件
+        m_Audio.clip = m_musicClip;//指定音效
+        m_Audio.loop = true;//循环播放
+        m_Audio.Play();//播放音乐
+
+        var restart_botton = canvas.transform.Find("Button").GetComponent<Button>();
+        restart_botton.onClick.AddListener(delegate ()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);//重新开始关卡
+        }
+
+        );
+            {
+
+        }
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
